fb5bba5fc816a5c7149e291b0faa0189acba998d
Primestoo <161174803+Primestoo@users.noreply.github.com>
2024-04-02 02:30:05 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Play background music
+const bgMusic = document.getElementById('bgMusic');
+bgMusic.play();
+
+// Function to pause background music when any video is played
+function pauseBgMusic() {
+  bgMusic.pause();
+}
