c9acb809740e7b0536f0ef20dd6f22989b75977e
Primestoo <161174803+Primestoo@users.noreply.github.com>
2024-04-02 06:19:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function toggleModal() {
   var modal = document.getElementById('myModal');
   modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+  console.log('Menu toggled');
 }
 
 // Function to go back to the previous page
